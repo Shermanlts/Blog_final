21f577855d130bec915c13bfa33c9e96e320aad8
Rob Sherman <Shermanlts@gmail.com>
2017-02-02 09:39:58 -0800

    Added all empty html files and added base code into app.

--- blog.py
+++ blog.py
@@ -10,3 +10,68 @@ import jinja2
 
 from google.appengine.ext import db
 
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(Loader=jinja2.FileSystemLoader(template_dir),
+                              autoescape=True)
+
+secret = 'monkey'
+
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+
+def make_secure(val):
+    return '%|%' % (val, hmac.new(secret, val).hexdigest())
+
+
+def check_secure(secure_val):
+    val = secure_val.split('|')[0]
+    if secure_val == make_secure(val):
+        return val
+
+
+class BlogHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        params['user'] = self.user
+        return render_str(template, **params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+    def set_secure_cookie(self, name, val):
+        cookie_val = make_secure(val)
+        self.response.headers.add_header(
+            'Set-Cookie',
+            '%s=%s; Path=/' % (name, cookie_val))
+
+    def read_secure_cookie(self, name):
+        cookie_val = self.request.cookies.get(name)
+        return cookie_val and check_secure(cookie_val)
+
+    def login(self, user):
+        self.set_secure_cookie('user_id', str(user.key().id()))
+
+    def logout(self):
+        self.response.headers.add_header('Set-Cookie', 'user_id=; path=/')
+
+    def initialize(self, *a, **kw):
+        webapp2.RequestHandler.initialize(self, *a, **kw)
+        uid - self.read_secure_cookie('user_id')
+        self.user = uid and User.by_id(int(uid))
+
+
+
+
+app = webapp2.WSGIApplication([('/?', MainPage),
+                               ('/([0-9]+', PostPage),
+                               ('/newpost', NewPost),
+                               ('/signup', Signup),
+                               ('/login', login),
+                               ('/logout', logout),
+                               ],
+                              debug=True)

c0ca11e8fd2f1d255a30b40fa78a0aa718863cea
Rob Sherman <Shermanlts@gmail.com>
2017-02-10 18:11:09 -0800

    Finall got the first simple page working.  This is the starting commit for building the site in full. This should be considered the first usable commit for testing.

--- blog.py
+++ blog.py
@@ -10,45 +10,45 @@ import jinja2
 
 from google.appengine.ext import db
 
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(Loader=jinja2.FileSystemLoader(template_dir),
-                               autoescape=True)
+template_dir = os.path.join(os.path.dirname(__file__), 'Templates')
+jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(template_dir),
+    autoescape=True
+    )
 
 secret = 'monkey'
 
 
-def render_str(template, **params):
-    t = jinja_env.get_template(template)
-    return t.render(params)
+# def render_str(template, **params):
+#    t = jinja_env.get_template(template)
+#    return t.render(params)
 
 
-def make_secure(val):
-    return '%|%' % (val, hmac.new(secret, val).hexdigest())
+# def make_secure(val):
+#    return '%|%' % (val, hmac.new(secret, val).hexdigest())
 
 
-def check_secure(secure_val):
-    val = secure_val.split('|')[0]
-    if secure_val == make_secure(val):
-        return val
+# def check_secure(secure_val):
+#    val = secure_val.split('|')[0]
+#    if secure_val == make_secure(val):
+#        return val
 
 
 class BlogHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
+    def render_str(self, template, **kw):
+        t = jinja_env.get_template(template)
+        return t.render(kw)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
 
 class MainPage(BlogHandler):
     def get(self):
         self.render('welcome.html')
-
-    def post(self):
-        login = self.request.get('login')
-        signup = self.request.get('signup')
-
-        if login:
-            self.redirect('/login')
-        elif signup:
-            self.redirect('/signup')
 
 
 class Signup(BlogHandler):

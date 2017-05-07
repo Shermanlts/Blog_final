7c72875214c29bf037b97ea8a0fad3594b56657b
Rob Sherman <Shermanlts@gmail.com>
2017-05-06 22:23:44 -0700

    Cleaned up comments and fixed app.yaml

--- blog.py
+++ blog.py
@@ -22,151 +22,200 @@ secret = "10ajwoc803nwW"
 #  Universal functions*****************************************************
 
 
-#  Renders output based upon template
 def render_strg(template, **params):
+    """Renders output based upon template
+
+    Args:
+        template: Incoming HTML templace
+        **params: any passed on parameters
+
+    Returns:
+        Calls render on the newly created object
+    """
     t = jinja_env.get_template(template)
     return t.render(params)
 
 
-#  Creates a hash to be used as a cookie
 def make_secure_val(val):
+    """Creates a hash to be used as a cookie
+
+    Args:
+        val: A string value for hashing
+
+    Returns:
+        constructed val,hashedval
+    """
     return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
 
 
-#  Checks an existing hash from a cookie to determine if valid
 def check_secure_val(secure_val):
+    """Checks an existing hash from a cookie to determine if valid
+
+    Args:
+        secure_val: val,hashedval object needing to be checked
+
+    Returns:
+        """
     val = secure_val.split('|')[0]
     if secure_val == make_secure_val(val):
         return val
 
 
-#  Creates a random salt of a length that defaults to 5 char
 def make_salt(length=5):
+    """Creates a random salt of a length that defaults to 5 char
+
+    Args:
+        Length: defaults at 5 but can be set longer
+
+    Returns:
+        String of random letters of length=X
+    """
     return ''.join(random.choice(letters) for x in xrange(length))
 
 
-#  Creates a password hash for secure DB storage
 def make_pw_hash(name, pw, salt=None):
+    """Creates a password hash for secure DB storage
+    
+    Args:
+        name: name of user
+        pw: password of user
+        salt: Any previously defined salt(defaults to none)
+
+    Returns:
+        salt,hash
+    """
     if not salt:
         salt = make_salt()
     h = hashlib.sha256(name + pw + salt).hexdigest()
     return '%s,%s' % (salt, h)
 
 
-#  Checks if a new username is valid
 def valid_username(username):
+    """Checks if a new username is valid"""
     USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
     return username and USER_RE.match(username)
 
 
-#  Checks if a new password is valid
 def valid_password(password):
+    """Checks if a new password is valid"""
     PASS_RE = re.compile(r"^.{3,20}$")
     return password and PASS_RE.match(password)
 
 
-#  Checks if incoming password is valid for a specific user
 def valid_pw(name, password, h):
+    """Checks if incoming password is valid for a specific user"""
     salt = h.split(',')[0]
     return h == make_pw_hash(name, password, salt)
 
 
 #  Blog and database classes **************************************************
-#  Basic blog functions which are imported into page classes
 class BlogHandler(webapp2.RequestHandler):
-    # Simplifies the write function
+    """Basic blog functions which are imported into page classes"""
     def write(self, *a, **kw):
+        """Simplifies the write function"""
         self.response.out.write(*a, **kw)
 
-    # Sends the render with template to the global function -line 25
     def render_str(self, template, **params):
+        """Sends the render with template to the global function -line 25"""
         return render_strg(template, **params)
 
-    # Builds content and sends to render_str
     def render(self, template, **kw):
+        """Builds content and sends to render_str"""
         self.write(self.render_str(template, **kw))
 
-    # Sets a hashed tracking cookie for a user
     def set_secure_cookie(self, name, val):
+        """Sets a hashed tracking cookie for a user"""
         cookie_val = make_secure_val(val)
         self.response.headers.add_header(
             'Set-Cookie',
             '%s=%s; Path=/' % (name, cookie_val))
 
-    # Checks if a cookie is valid
     def read_secure_cookie(self, name):
+        """Checks if a cookie is valid"""
         cookie_val = self.request.cookies.get(name)
         return cookie_val and check_secure_val(cookie_val)
 
-    # Login function setting cookie and page redirect
     def login(self, user):
+        """Login function setting cookie and page redirect"""
         self.set_secure_cookie('user_id', str(user.key().id()))
         self.redirect('/welcome')
 
-    # Logout function clears cookie and redirects to home
     def logout(self):
+        """Logout function clears cookie and redirects to home"""
         self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
         self.redirect('/')
 
-    # Used to check a users login status on every page
     def initialize(self, *a, **kw):
+        """Used to check a users login status on every page"""
         webapp2.RequestHandler.initialize(self, *a, **kw)
         uid = self.read_secure_cookie('user_id')
         self.user = uid and User.by_id(int(uid))
 
 
-#  Defines User database and gives it default functions
 class User(db.Model):
-    # Sets the user DB
+    """Defines User database and gives it default functions"""
+    """Sets the user DB"""
     name = db.StringProperty(required=True)
     pw_hash = db.StringProperty(required=True)
     email = db.StringProperty()
 
-    # Checks if a uid is valid
     @classmethod
     def by_id(cls, uid):
+        """Returns a user file found by id"""
         return User.get_by_id(uid)
 
-    # Checks if a name is valid
     @classmethod
     def by_name(cls, name):
+        """Returns a user file found by name"""
         u = User.all().filter('name =', name).get()
         return u
 
-    # Creates a pw hash and returns a built user structure
     @classmethod
     def register(cls, name, pw, email=None):
+        """Creates a pw hash and returns a built user structure"""
         pw_hash = make_pw_hash(name, pw)
         return User(name=name,
                     pw_hash=pw_hash,
                     email=email)
 
-    # Checks if username and password are valid
     @classmethod
     def login(cls, name, pw):
+        """Checks if username and password are valid"""
         u = cls.by_name(name)
         if u and valid_pw(name, pw, u.pw_hash):
             return u
 
 
-# defines a comments DB for posts
+
 class Comment(db.Model):
-    # Sets the comments DB
+    """defines a comments DB for posts
+    Attr:
+        postID: ID number of post comment is for
+        commenter: user who wrote the comment
+        comment: comment text
+        created: date of comment creation
+        CID: Comment KeyID held more accessible for templates
+        """
     postID = db.StringProperty(required=True)
     commenter = db.StringProperty(required=True)
     comment = db.TextProperty(required=True)
     created = db.DateTimeProperty(auto_now_add=True)
-    # CID is used to hold the keyID more accessible for template use
     CID = db.StringProperty()
 
-    # Formats text so that returns show in html reprint
     def render(self):
+        """Formats text so that returns show in html reprint"""
         self._render_text = self.comment.replace('\n', '<br>')
         return render_strg("comment.html", p=self)
 
 
-#  Defines user post database
 class Post(db.Model):
+    """Defines user post database
+    Attr:
+        subject: Subject of post
+        content: Post content
+        created: Date of post creation
+        poster: User who created post
+        """
     subject = db.StringProperty(required=True)
     content = db.TextProperty(required=True)
     created = db.DateTimeProperty(auto_now_add=True)
@@ -175,8 +224,8 @@ class Post(db.Model):
     liked_by = db.ListProperty(str)
     perma_link = db.StringProperty()
 
-    # Formats text so that returns show in html reprint
     def render(self):
+        """Formats text so that returns show in html reprint"""
         self._render_text = self.content.replace('\n', '<br>')
         return render_strg("post.html", p=self)
 
@@ -184,8 +233,8 @@ class Post(db.Model):
 #  Blog page handlers**********************************************************
 #  All blog pages that require a login have the if self.user checking for login
 
-# Main blog page handler /blog
 class BlogFront(BlogHandler):
+    """Main blog page handler /blog"""
     def get(self):
         if self.user:
             posts = Post.all().order('-created')
@@ -194,10 +243,10 @@ class BlogFront(BlogHandler):
             self.redirect('/')
 
 
-# Handles editing of posts "/edit/"
 class CEditPost(BlogHandler):
+    """Handles editing of posts "/edit/"""
     def get(self, post_id):
-        # if self.user used to ensure user is logged in throughout code
+        """if self.user used to ensure user is logged in throughout code"""
         if self.user:
             key = db.Key.from_path('Comment', int(post_id))
             c = db.get(key)
@@ -222,33 +271,33 @@ class CEditPost(BlogHandler):
                         error=error)
 
 
-# Delete a Comment
 class CDeletePost(BlogHandler):
+    """Delete a Comment"""
     def get(self, post_id):
         if self.user:
             key = db.Key.from_path('Comment', int(post_id))
             comment = db.get(key)
             post = str(comment.postID)
             comment.delete()
-            # redirect was happening before delete completed so sleep added
+            """redirect was happening before delete completed so sleep added"""
             sleep(1)
             self.redirect('/%s' % post)
 
 
-# Delete a post
 class DeletePost(BlogHandler):
+    """Delete a post"""
     def get(self, post_id):
         if self.user:
             key = db.Key.from_path('Post', int(post_id))
             post = db.get(key)
             post.delete()
-            # redirect was happening before delete completed so sleep added
+            """redirect was happening before delete completed so sleep added"""
             sleep(1)
             self.redirect('/')
 
 
-# Handles editing of posts "/edit/"
 class EditPost(BlogHandler):
+    """Handles editing of posts "/edit/"""
     def get(self, post_id):
         if self.user:
             key = db.Key.from_path('Post', int(post_id))
@@ -278,8 +327,8 @@ class EditPost(BlogHandler):
                         error=error)
 
 
-# Handles the "/Login" page
 class Login(BlogHandler):
+    """Handles the "/Login" page"""
     def get(self):
         self.render("login.html")
 
@@ -296,14 +345,14 @@ class Login(BlogHandler):
             self.render('login.html', error=msg)
 
 
-# Logout function
 class Logout(BlogHandler):
+    """Logout function"""
     def get(self):
         self.logout()
 
 
-# Handles intro page "/"
 class MainPage(BlogHandler):
+    """Handles intro page "/"""
     def get(self):
         if self.user:
             self.redirect('/blog')
@@ -311,8 +360,8 @@ class MainPage(BlogHandler):
             self.render("start.html")
 
 
-# Handles new posts "/newpost"
 class NewPost(BlogHandler):
+    """Handles new posts "/newpost"""
     def get(self):
         if self.user:
             self.render("newpost.html")
@@ -335,8 +384,8 @@ class NewPost(BlogHandler):
                         error=error)
 
 
-# Handles permanent linked posts "/([0-9]+)"
 class PostPage(BlogHandler):
+    """Handles permanent linked posts "/([0-9]+)"""
     def get(self, post_id):
         if self.user:
             key = db.Key.from_path('Post', int(post_id))
@@ -358,7 +407,7 @@ class PostPage(BlogHandler):
             self.redirect('/')
 
     def post(self, post_id):
-        # used for new comments
+        """used for new comments"""
         key = db.Key.from_path('Post', int(post_id))
         user = self.user.name
         if self.request.get('comment'):
@@ -369,14 +418,12 @@ class PostPage(BlogHandler):
             c.put()
             sleep(1)
             self.redirect('/%s' % post_id)
-        # used for new likes
         elif self.request.get('like'):
-            p = db.get(key)  
+            p = db.get(key)
             p.liked_by.append(user)
             p.put()
             sleep(1)
             self.redirect(p.perma_link)
-        # used for errors
         else:
             posts = Comment.all().filter('postID =', post_id).order('-created')
             user = self.user.name
@@ -385,8 +432,8 @@ class PostPage(BlogHandler):
                         posts=posts, error=error)
 
 
-# Handles the singup page "/signup"
 class Signup(BlogHandler):
+    """Handles the singup page "/signup"""
     def get(self):
         self.render("signup.html")
 
@@ -421,15 +468,15 @@ class Signup(BlogHandler):
             self.redirect('/welcome')
 
 
-# Handles the "/welcome" page. Users are sent there upon login/signup
 class Welcome(BlogHandler):
+    """Handles the "/welcome" page. Users are sent there upon login/signup"""
     def get(self):
         if self.user:
             self.render("welcome.html", name=self.user.name)
         else:
             self.redirect('/')
 
-# Below tells the application which function to send a specific web address
+"""Below tells the application which function to send a specific web address"""
 app = webapp2.WSGIApplication([("/", MainPage),
                                ("/blog", BlogFront),
                                ("/([0-9]+)", PostPage),
